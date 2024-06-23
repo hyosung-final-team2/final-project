9c7394c00bc84be0482ca7e8d313778cd24750f0
hamings <113500975+hamings@users.noreply.github.com>
2024-06-23 20:20:58 +0900

    project init

---
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/Ubun2BeApplication.java
@@ -0,0 +1,13 @@
+package kr.or.kosa.ubun2_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ubun2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ubun2BeApplication.class, args);
+	}
+
+}
---
+++ ubun2_be/src/test/java/kr/or/kosa/ubun2_be/Ubun2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.kosa.ubun2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ubun2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
