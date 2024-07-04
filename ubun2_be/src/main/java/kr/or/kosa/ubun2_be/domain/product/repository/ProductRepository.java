3607c3c7d27c027f36ff01e752461a6d3051049e
정혜미 <113500975+hamings@users.noreply.github.com>
2024-07-04 18:51:18 +0900

    KAN-25 refactor: 메서드 삭제

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/repository/ProductRepository.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/repository/ProductRepository.java
@@ -3,12 +3,9 @@ package kr.or.kosa.ubun2_be.domain.product.repository;
 import kr.or.kosa.ubun2_be.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCustomerCustomerId(Long customerId);
-
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
     Optional<Product> findByCustomerCustomerIdAndProductId(Long customerId, Long productId);
 
     boolean existsByProductName(String productName);
