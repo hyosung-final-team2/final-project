3653a38faf571db1da72c9a3772836a139031aaa
기정김 <404.notfound@kakao.com>
2024-07-04 21:50:36 +0900

    style: button style 수정

--- ubun2_fe/src/customer/components/AddressList/AddressTableFeature.jsx
+++ ubun2_fe/src/customer/components/AddressList/AddressTableFeature.jsx
@@ -2,24 +2,16 @@ import React from 'react';
 import SearchInput from '../common/Input/SearchInput';
 
 const AddressTableFeature = () => {
+  const commonButtonStyles = 'px-4 py-2 rounded-lg transition duration-200 border border-gray-200 shadow-md';
+
   return (
     <div className='flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 p-4 bg-white dark:bg-gray-900'>
       <div className='flex'>
         <SearchInput placeholder='회원 검색' />
       </div>
       <div className='flex space-x-2'>
-        <button
-          type='button'
-          className='text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800'
-        >
-          주소 등록
-        </button>
-        <button
-          type='button'
-          className='text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900'
-        >
-          삭제
-        </button>
+        <button className={`${commonButtonStyles} bg-white text-gray-600 hover:text-main hover:bg-slate-50`}>주소 등록</button>
+        <button className={`${commonButtonStyles} bg-red-300 text-red-700 hover:text-white hover:bg-red-500 px-8`}>삭제</button>
       </div>
     </div>
   );
