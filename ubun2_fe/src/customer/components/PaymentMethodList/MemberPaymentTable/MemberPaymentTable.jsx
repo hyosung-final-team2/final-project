2643e55394a62a692f2fa1f9509f665067a21401
기정김 <404.notfound@kakao.com>
2024-07-04 15:39:32 +0900

    style : 모달 내 글씨 색 변경

--- ubun2_fe/src/customer/components/PaymentMethodList/MemberPaymentTable/MemberPaymentTable.jsx
+++ ubun2_fe/src/customer/components/PaymentMethodList/MemberPaymentTable/MemberPaymentTable.jsx
@@ -6,8 +6,8 @@ import TableHead from '../../common/Table/TableHead';
 
 const MemberPaymentTable = ({ payments, title }) => {
   return (
-    <div className='p-3'>
-      <h2 className='text-2xl font-bold mb-3'>{title}</h2>
+    <div className='p-2'>
+      <h1 className='text-2xl font-bold mb-3 text-main'>{title}</h1>
       <Table hoverable>
         <TableHead tableColumns={tableColumn.paymentMethod.detail} isCheckable={false} />
         <TableBody users={payments} TableRowComponent={MemberPaymentTableRow} isCheckable={false} />
