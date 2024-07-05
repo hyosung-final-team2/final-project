6e10cfc8bfdc00e769dc04c91df7c10d16f92e01
기정김 <404.notfound@kakao.com>
2024-07-05 14:39:48 +0900

    chore: 버튼 onclick 추가&스타일 변경

--- ubun2_fe/src/customer/components/AddressList/AddressTableFeature.jsx
+++ ubun2_fe/src/customer/components/AddressList/AddressTableFeature.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import SearchInput from '../common/Input/SearchInput';
 
-const AddressTableFeature = () => {
+const AddressTableFeature = ({ setOpenModal }) => {
   const commonButtonStyles = 'px-4 py-2 rounded-lg transition duration-200 border border-gray-200 shadow-md';
 
+  const handleClick = () => {
+    setOpenModal(true);
+  };
   return (
     <div className='flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 p-4 bg-white dark:bg-gray-900'>
       <div className='flex'>
         <SearchInput placeholder='회원 검색' />
       </div>
       <div className='flex space-x-2'>
-        <button className={`${commonButtonStyles} bg-white text-gray-600 hover:text-main hover:bg-slate-50`}>주소 등록</button>
+        <button className={`${commonButtonStyles} bg-white text-gray-600 hover:text-main hover:bg-slate-50`} onClick={handleClick}>
+          주소 등록
+        </button>
         <button className={`${commonButtonStyles} bg-red-300 text-red-700 hover:text-white hover:bg-red-500 px-8`}>삭제</button>
       </div>
     </div>
--- ubun2_fe/src/customer/components/common/Dropdown/Dropdown.jsx
+++ ubun2_fe/src/customer/components/common/Dropdown/Dropdown.jsx
@@ -1,37 +1,34 @@
-import { Button } from 'flowbite-react';
+import React from 'react';
 
-const Dropdown = ({ label }) => {
+const Dropdown = ({ label, items, handleDatas }) => {
+  const handleOnClick = item => {
+    handleDatas(item.value);
+  };
+
   return (
-    <div className='flex border-solid border-gray-300 items-center'>
-      <Button color='gray' id='dropdownDefaultButton' data-dropdown-toggle='dropdown' type='button'>
-        {label}{' '}
-        <svg className='w-2.5 h-2.5 ms-3 self-center' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 10 6'>
+    <div className='flex items-center'>
+      <button
+        id='dropdownDefaultButton'
+        data-dropdown-toggle='dropdown'
+        type='button'
+        className='shadow-md border-gray-300 bg-white text-black font-medium rounded-lg text-sm px-4 py-2.5 inline-flex items-center'
+      >
+        {label}
+        <svg className='w-2.5 h-2.5 ml-3' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 10 6'>
           <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m1 1 4 4 4-4' />
         </svg>
-      </Button>
-
+      </button>
       <div id='dropdown' className='z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700'>
         <ul className='py-2 text-sm text-gray-700 dark:text-gray-200' aria-labelledby='dropdownDefaultButton'>
-          <li>
-            <a href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
-              Settings
-            </a>
-          </li>
-          <li>
-            <a href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
-              Earnings
-            </a>
-          </li>
-          <li>
-            <a href='#' className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
-              Sign out
-            </a>
-          </li>
+          {items.map((item, index) => (
+            <li
+              key={index}
+              className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer'
+              onClick={() => handleOnClick(item)}
+            >
+              {item.label}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
