c3e5a17a482072e46206e803c921457684ad0b1c
정혜미 <113500975+hamings@users.noreply.github.com>
2024-07-06 17:26:10 +0900

    KAN-25 refactor: setter 제거

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/dto/ProductDeleteRequest.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/dto/ProductDeleteRequest.java
@@ -1,10 +1,8 @@
 package kr.or.kosa.ubun2_be.domain.product.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class ProductDeleteRequest {
     private Long productId;
 }
