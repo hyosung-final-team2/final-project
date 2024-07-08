e3c1c060aa539230df1414270e9a3930c6b3f251
기정김 <404.notfound@kakao.com>
2024-07-08 17:25:17 +0900

    refactor: 조회성능 고려 QueryDsl 사용 RepositoryImpl, RepositoryCustom 구현

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/repository/AddressRepository.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/repository/AddressRepository.java
@@ -4,5 +4,5 @@ import kr.or.kosa.ubun2_be.domain.address.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface AddressRepository extends JpaRepository<Address, Long>, QuerydslPredicateExecutor<Address> {
+public interface AddressRepository extends JpaRepository<Address, Long>, AddressRepositoryCustom {
 }
