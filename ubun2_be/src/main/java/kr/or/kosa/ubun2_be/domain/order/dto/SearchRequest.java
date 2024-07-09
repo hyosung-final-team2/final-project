0090fa359bc54711c2294c09f89794f099032ae6
이소민 <alsth712@naver.com>
2024-07-09 15:52:59 +0900

    KAN-76 feat: 기존 서비스 로직 수정 및 검색 추가

---
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/dto/SearchRequest.java
@@ -0,0 +1,11 @@
+package kr.or.kosa.ubun2_be.domain.order.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchRequest {
+    private String searchCategory;
+    private String searchKeyword;
+}
--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/service/OrderService.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/service/OrderService.java
@@ -1,12 +1,13 @@
 package kr.or.kosa.ubun2_be.domain.order.service;
 
-import kr.or.kosa.ubun2_be.domain.order.dto.OrderResponse;
-import kr.or.kosa.ubun2_be.domain.order.dto.SubscriptionOrderResponse;
+import kr.or.kosa.ubun2_be.domain.order.dto.SearchRequest;
+import kr.or.kosa.ubun2_be.domain.order.dto.UnifiedOrderResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface OrderService {
-    Page<OrderResponse> getOrders(Long customerId, Pageable pageable);
 
-    Page<SubscriptionOrderResponse> getSubscriptionOrders(Long customerId, Pageable pageable);
+
+    Page<UnifiedOrderResponse> getOrders(Long customerId, SearchRequest searchRequest, Pageable pageable);
+
 }
