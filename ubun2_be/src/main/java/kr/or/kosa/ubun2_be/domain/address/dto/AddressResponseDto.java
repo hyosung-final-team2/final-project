4e86bcb4acfe8de05ab1d12dbd0e305f631a031f
기정김 <404.notfound@kakao.com>
2024-07-09 15:24:08 +0900

    KAN-82 refactor: 클래스 이름 수정

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/dto/AddressResponseDto.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/dto/AddressResponseDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AddressDto {
+public class AddressResponseDto {
     private Long addressId;
     private String address;
 }
--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/repository/impl/AddressRepositoryImpl.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/repository/impl/AddressRepositoryImpl.java
@@ -4,12 +4,11 @@ import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
-import kr.or.kosa.ubun2_be.domain.address.dto.AddressDto;
+import kr.or.kosa.ubun2_be.domain.address.dto.AddressResponseDto;
 import kr.or.kosa.ubun2_be.domain.address.dto.AddressMemberInfoResponse;
 import kr.or.kosa.ubun2_be.domain.address.dto.AddressResponse;
 import kr.or.kosa.ubun2_be.domain.address.entity.QAddress;
 import kr.or.kosa.ubun2_be.domain.address.repository.AddressRepositoryCustom;
-import kr.or.kosa.ubun2_be.domain.member.entity.QMember;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -17,8 +16,10 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository;
 import kr.or.kosa.ubun2_be.domain.address.entity.Address;
 
-
 import java.util.List;
+
+import static kr.or.kosa.ubun2_be.domain.address.entity.QAddress.address1;
+import static kr.or.kosa.ubun2_be.domain.member.entity.QMember.member;
 
 
 @Repository
@@ -27,12 +28,9 @@ public class AddressRepositoryImpl extends QuerydslRepositorySupport implements 
     public AddressRepositoryImpl() {
         super(Address.class);
     }
-
     @Override
     public Page<AddressResponse> findAllAddressesWithMember(Pageable pageable) {
         QAddress address = new QAddress("address");
-        QMember member = new QMember("member");
-
         JPQLQuery<AddressResponse> query = from(address)
                 .join(address.member, member)
                 .select(Projections.constructor(AddressResponse.class,
@@ -51,15 +49,12 @@ public class AddressRepositoryImpl extends QuerydslRepositorySupport implements 
 
         return new PageImpl<>(content, pageable, total);
     }
-
     @Override
     public AddressMemberInfoResponse findMemberInfoByAddressId(Long addressId) {
-        QAddress address = new QAddress("address");
-        QMember member = new QMember("member");
 
-        Tuple memberInfo = from(address)
-                .join(address.member, member)
-                .where(address.addressId.eq(addressId))
+        Tuple memberInfo = from(address1)
+                .join(address1.member, member)
+                .where(address1.addressId.eq(addressId))
                 .select(member.memberName, member.memberPhone, member.memberEmail, member.createdAt)
                 .fetchOne();
 
@@ -68,15 +63,15 @@ public class AddressRepositoryImpl extends QuerydslRepositorySupport implements 
         }
 
         // 해당 회원의 모든 주소 목록 조회
-        List<AddressDto> addresses = from(address)
-                .where(address.member.memberId.eq(
-                        JPAExpressions.select(address.member.memberId)
-                                .from(address)
-                                .where(address.addressId.eq(addressId))
+        List<AddressResponseDto> addresses = from(address1)
+                .where(address1.member.memberId.eq(
+                        JPAExpressions.select(address1.member.memberId)
+                                .from(address1)
+                                .where(address1.addressId.eq(addressId))
                 ))
-                .select(Projections.constructor(AddressDto.class,
-                        address.addressId,
-                        address.address))
+                .select(Projections.constructor(AddressResponseDto.class,
+                        address1.addressId,
+                        address1.address))
                 .fetch();
 
 
