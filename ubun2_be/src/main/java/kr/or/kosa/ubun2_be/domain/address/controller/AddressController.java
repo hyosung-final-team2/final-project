b50943b67f1862c922390d18a5b3dd0e55ce0455
기정김 <404.notfound@kakao.com>
2024-07-09 09:51:57 +0900

    KAN-82 feat: 주소아이디로 회원정보 조회 컨트롤러 & DTO

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/controller/AddressController.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/controller/AddressController.java
@@ -1,7 +1,8 @@
 package kr.or.kosa.ubun2_be.domain.address.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import kr.or.kosa.ubun2_be.domain.address.dto.AddressRequest;
+import kr.or.kosa.ubun2_be.domain.address.dto.AddressMemberDetailRequest;
+import kr.or.kosa.ubun2_be.domain.address.dto.AddressMemberInfoResponse;
 import kr.or.kosa.ubun2_be.domain.address.dto.AddressResponse;
 import kr.or.kosa.ubun2_be.domain.address.service.AddressService;
 import kr.or.kosa.ubun2_be.global.dto.ResponseDto;
@@ -10,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,15 +23,19 @@ public class AddressController {
 
     @Operation(summary = "전체 주소 목록 조회")
     @GetMapping("/")
-    public ResponseDto<?> getAddresses(
-                        @PageableDefault(size=PAGE_SIZE, sort = SORT_DEFAULT, direction = Sort.Direction.DESC) Pageable pageable) {
-        Page<AddressResponse> addressResponsesList = addressService.getAllAddresses(pageable);
-        return ResponseDto.ok(addressResponsesList,"정상출력 데이터");
     public ResponseDto<?> getAllAddresses(
             @PageableDefault(size = PAGE_SIZE, sort = "addressId", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<AddressResponse> addresses = addressService.getAllAddresses(pageable);
         return ResponseDto.ok(addresses, "주소 목록을 성공적으로 조회했습니다.");
     }
+
+    @Operation(summary = "주소아이디로 회원 정보 조회")
+    @GetMapping("/{address_id}")
+    public ResponseDto<?> getMemberAddressInfo(@PathVariable("address_id") Long addressId) {
+        AddressMemberDetailRequest addressMemberDetailRequest = AddressMemberDetailRequest.builder()
+                .addressId(addressId).build();
+        AddressMemberInfoResponse response = addressService.getMemberInfoByAddressId(addressMemberDetailRequest);
+        return ResponseDto.ok(response, "주소 상세를 성공적으로 조회했습니다.");
     }
 
 }
---
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/dto/AddressDto.java
@@ -0,0 +1,15 @@
+package kr.or.kosa.ubun2_be.domain.address.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressDto {
+    private Long addressId;
+    private String address;
+}
