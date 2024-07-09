f5028945e2fb89ab420f00022b8f2e6632fc5a41
이소민 <alsth712@naver.com>
2024-07-09 15:52:11 +0900

    KAN-76 refactor: 기존 쿼리문 수정 및 검색 추가

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/repository/OrderRepository.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/repository/OrderRepository.java
@@ -1,47 +1,7 @@
 package kr.or.kosa.ubun2_be.domain.order.repository;
 
 import kr.or.kosa.ubun2_be.domain.order.entity.Order;
-import kr.or.kosa.ubun2_be.domain.order.entity.SubscriptionOrder;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-    String PENDING_STATUS = "kr.or.kosa.ubun2_be.domain.product.enums.OrderStatus.PENDING";
-
-    @Query(value = "SELECT DISTINCT o FROM Order o " +
-            "LEFT JOIN FETCH o.member m " +
-            "JOIN m.memberCustomers mc " +
-            "LEFT JOIN FETCH o.orderProducts op " +
-            "LEFT JOIN FETCH o.paymentMethod pm " +
-            "LEFT JOIN AccountPayment ap ON pm.paymentMethodId = ap.paymentMethodId " +
-            "LEFT JOIN CardPayment cp ON pm.paymentMethodId = cp.paymentMethodId " +
-            "WHERE mc.customer.customerId = :customerId " +
-            "AND o.orderStatus <> " + PENDING_STATUS,
-            countQuery = "SELECT COUNT(DISTINCT o) FROM Order o " +
-                    "JOIN o.member m " +
-                    "JOIN m.memberCustomers mc " +
-                    "WHERE mc.customer.customerId = :customerId " +
-                    "AND o.orderStatus <> " + PENDING_STATUS)
-    Page<Order> getOrders(@Param("customerId") Long customerId, Pageable pageable);
-
-    @Query(value = "SELECT DISTINCT so FROM SubscriptionOrder so " +
-            "LEFT JOIN FETCH so.member m " +
-            "JOIN m.memberCustomers mc " +
-            "LEFT JOIN FETCH so.subscriptionOrderProducts sop " +
-            "LEFT JOIN FETCH so.paymentMethod pm " +
-            "LEFT JOIN AccountPayment ap ON pm.paymentMethodId = ap.paymentMethodId " +
-            "LEFT JOIN CardPayment cp ON pm.paymentMethodId = cp.paymentMethodId " +
-            "WHERE mc.customer.customerId = :customerId " +
-            "AND so.orderStatus <> " + PENDING_STATUS,
-            countQuery = "SELECT COUNT(DISTINCT so) FROM SubscriptionOrder so " +
-                    "JOIN so.member m " +
-                    "JOIN m.memberCustomers mc " +
-                    "WHERE mc.customer.customerId = :customerId " +
-                    "AND so.orderStatus <> " + PENDING_STATUS)
-    Page<SubscriptionOrder> getSubscriptionOrders(@Param("customerId") Long customerId, Pageable pageable);
-
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 }
