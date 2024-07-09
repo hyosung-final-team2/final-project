14ebb73bc9d40002ed00d3f33e1ce03728f3b211
이소민 <alsth712@naver.com>
2024-07-09 15:53:17 +0900

    KAN-76 feat: 기존 컨트롤러 수정 및 검색 추가

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/controller/OrderCustomerController.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/controller/OrderCustomerController.java
@@ -1,38 +1,36 @@
 package kr.or.kosa.ubun2_be.domain.order.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import kr.or.kosa.ubun2_be.domain.order.dto.OrderResponse;
-import kr.or.kosa.ubun2_be.domain.order.dto.SubscriptionOrderResponse;
+import kr.or.kosa.ubun2_be.domain.order.dto.SearchRequest;
+import kr.or.kosa.ubun2_be.domain.order.dto.UnifiedOrderResponse;
 import kr.or.kosa.ubun2_be.domain.order.service.OrderService;
 import kr.or.kosa.ubun2_be.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.*;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/customers/orders")
 public class OrderCustomerController {
     private final OrderService orderService;
+    private static final String SORT_DEFAULT = "createdAt";
+
 
     private static final int PAGE_SIZE = 9;
 
-    @Operation(summary = "전체 단건 주문 목록 조회")
+    @Operation(summary = "전체 주문 목록 조회 (일반 주문 + 구독 주문)")
     @GetMapping("/")
-    public ResponseDto<?> findOrders(@RequestParam("customerId") Long customerId, @PageableDefault(size = PAGE_SIZE, sort = "orderId", direction = Sort.Direction.DESC) Pageable pageable) {
-        Page<OrderResponse> orderResponseList = orderService.getOrders(customerId, pageable);
+    public ResponseDto<?> findOrders(
+            @RequestParam("customerId") Long customerId,
+            SearchRequest searchRequest,
+            @PageableDefault(size = PAGE_SIZE, sort = SORT_DEFAULT, direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<UnifiedOrderResponse> orderResponseList = orderService.getOrders(customerId, searchRequest, pageable);
         return ResponseDto.ok(orderResponseList, "정상출력 데이터");
     }
 
-    @Operation(summary = "전체 정기 주문 목록 조회")
-    @GetMapping("/subscription")
-    public ResponseDto<?> findSubscriptionOrders(@RequestParam("customerId") Long customerId, @PageableDefault(size = PAGE_SIZE, sort = "subscriptionOrderId", direction = Sort.Direction.DESC) Pageable pageable) {
-        Page<SubscriptionOrderResponse> subscriptionOrderResponseList = orderService.getSubscriptionOrders(customerId, pageable);
-        return ResponseDto.ok(subscriptionOrderResponseList, "정상출력 데이터");
-    }
+
 
 }
