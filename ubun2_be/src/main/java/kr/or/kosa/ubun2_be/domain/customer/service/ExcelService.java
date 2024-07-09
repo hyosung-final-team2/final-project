d448b97a4687aae86936d53662d86115f4a51d31
윤동훈 <acttoze1@gmail.com>
2024-07-09 14:16:01 +0900

    KAN-47 feat: 메서드 어노테이션 추가

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/customer/service/ExcelService.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/customer/service/ExcelService.java
@@ -8,6 +8,7 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
@@ -38,6 +39,7 @@ public class ExcelService {
         return new ByteArrayResource(out.toByteArray());
     }
 
+    @Transactional
     public void registerExcel(Long customerId, MultipartFile file) throws IOException {
         List<String[]> data = parseExcel(file);
         registerMembersFromData(data, customerId);
