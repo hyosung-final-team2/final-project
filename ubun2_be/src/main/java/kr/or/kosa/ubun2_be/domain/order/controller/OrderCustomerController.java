539fb6717b8ae3eb34ee4dbf75aa92fae263eb7c
이소민 <alsth712@naver.com>
2024-07-10 22:02:45 +0900

    KAN-80 feat: customerId 관련 controller 변경

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/controller/OrderCustomerController.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/controller/OrderCustomerController.java
@@ -3,12 +3,14 @@ package kr.or.kosa.ubun2_be.domain.order.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import kr.or.kosa.ubun2_be.domain.order.dto.*;
 import kr.or.kosa.ubun2_be.domain.order.service.OrderService;
+import kr.or.kosa.ubun2_be.global.auth.model.CustomUserDetails;
 import kr.or.kosa.ubun2_be.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,53 +23,54 @@ public class OrderCustomerController {
 
     @Operation(summary = "전체 주문 목록 조회 (일반 주문 + 구독 주문)")
     @GetMapping("/")
-    public ResponseDto<?> getOrders(
-            @RequestParam("customerId") Long customerId,
-            SearchRequest searchRequest,
-            @PageableDefault(size = PAGE_SIZE, sort = SORT_DEFAULT, direction = Sort.Direction.DESC) Pageable pageable) {
-
-        Page<UnifiedOrderResponse> orderResponseList = orderService.getOrders(customerId, searchRequest, pageable);
+    public ResponseDto<?> getOrders(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                    SearchRequest searchRequest,
+                                    @PageableDefault(size = PAGE_SIZE, sort = SORT_DEFAULT, direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<UnifiedOrderResponse> orderResponseList = orderService.getOrders(customUserDetails.getUserId(), searchRequest, pageable);
         return ResponseDto.ok(orderResponseList, "정상출력 데이터");
     }
 
     @Operation(summary = "단건 주문 상세 조회")
-    @GetMapping("{order_id}")
-    public ResponseDto<?> getOrderByOrderId(@PathVariable("order_id") Long orderId, Long customerId) {
-        OrderDetailResponse orderDetailResponse = orderService.getOrderByCustomerIdAndOrderId(orderId, customerId);
+    @GetMapping("/{order_id}")
+    public ResponseDto<?> getOrderByOrderId(@PathVariable("order_id") Long orderId,
+                                            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        OrderDetailResponse orderDetailResponse = orderService.getOrderByCustomerIdAndOrderId(orderId, customUserDetails.getUserId());
         return ResponseDto.ok(orderDetailResponse, "정상출력 데이터");
     }
 
     @Operation(summary = "정기 주문 상세 조회")
     @GetMapping("/subscription/{order_id}/{cycle_number}")
     public ResponseDto<?> getSubscriptionOrderByOrderId(@PathVariable("order_id") Long orderId,
-                                                        @RequestParam Long customerId,
+                                                        @AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                         @PathVariable("cycle_number") int cycleNumber) {
-        SubscriptionOrderDetailResponse response = orderService.getSubscriptionOrderByCustomerIdAndOrderIdAndCycleNumber(orderId, customerId, cycleNumber);
+        SubscriptionOrderDetailResponse response = orderService.getSubscriptionOrderByCustomerIdAndOrderIdAndCycleNumber(orderId, customUserDetails.getUserId(), cycleNumber);
         return ResponseDto.ok(response, "정상출력 데이터");
     }
 
     @Operation(summary = "전체 주문 대기 목록 조회 (일반 단건 주문 + 구독 주문)")
     @GetMapping("/pending")
-    public ResponseDto<?> getPendingOrders(@RequestParam("customerId") Long customerId,
+    public ResponseDto<?> getPendingOrders(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                            SearchRequest searchRequest,
                                            @PageableDefault(size = PAGE_SIZE, sort = SORT_DEFAULT, direction = Sort.Direction.DESC) Pageable pageable) {
 
-        Page<UnifiedOrderResponse> orderResponseList = orderService.getPendingOrders(customerId, searchRequest, pageable);
+        Page<UnifiedOrderResponse> orderResponseList = orderService.getPendingOrders(customUserDetails.getUserId(), searchRequest, pageable);
         return ResponseDto.ok(orderResponseList, "정상출력 데이터");
     }
 
 
     @Operation(summary = "대기 단건 주문 승인, 취소")
     @PutMapping("/approve/{order_id}")
-    public ResponseDto<?> updateOrderApprove(@RequestBody OrderApproveRequest orderApproveRequest, Long customerId){
-        orderService.updateOrderApprove(customerId, orderApproveRequest);
+    public ResponseDto<?> updateOrderApprove(@RequestBody OrderApproveRequest orderApproveRequest,
+                                             @AuthenticationPrincipal CustomUserDetails customUserDetails){
+        orderService.updateOrderApprove(customUserDetails.getUserId(), orderApproveRequest);
         return ResponseDto.ok(null, "정상출력 데이터");
     }
 
     @Operation(summary = "대기 정기 주문(최초 정기주문만) 승인, 취소")
     @PutMapping("/subscription/approve/{order_id}")
-    public ResponseDto<?> updateSubscriptionOrderApprove(@RequestBody SubscriptionApproveRequest subscriptionApproveRequest, Long customerId){
-        orderService.updateSubscriptionOrderApprove(customerId, subscriptionApproveRequest);
+    public ResponseDto<?> updateSubscriptionOrderApprove(@RequestBody SubscriptionApproveRequest subscriptionApproveRequest,
+                                                         @AuthenticationPrincipal CustomUserDetails customUserDetails){
+        orderService.updateSubscriptionOrderApprove(customUserDetails.getUserId(), subscriptionApproveRequest);
         return ResponseDto.ok(null, "정상출력 데이터");
     }
 
