c842824d7abd3a928bdd95ea2343234b054f2e55
기정김 <404.notfound@kakao.com>
2024-07-10 16:00:41 +0900

    KAN-71 refactor: repository에서는 entity를 반환 & querydsl 전체 수정

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/repository/AddressRepositoryCustom.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/repository/AddressRepositoryCustom.java
@@ -1,12 +1,12 @@
 package kr.or.kosa.ubun2_be.domain.address.repository;
 
-import kr.or.kosa.ubun2_be.domain.address.dto.AddressMemberInfoResponse;
-import kr.or.kosa.ubun2_be.domain.address.dto.AddressResponse;
 import kr.or.kosa.ubun2_be.domain.address.entity.Address;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface AddressRepositoryCustom {
-    Page<AddressResponse> findAllAddressesWithMember(Pageable pageable);
-    AddressMemberInfoResponse findMemberInfoByAddressId(Long addressId);
+    Page<Address> findAllAddressesWithMember(Pageable pageable, Long customerId);
+    Optional<Address> findAddressByIdAndCustomerId(Long addressId, Long customerId);
 }
--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/repository/impl/AddressRepositoryImpl.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/address/repository/impl/AddressRepositoryImpl.java
@@ -1,12 +1,7 @@
 package kr.or.kosa.ubun2_be.domain.address.repository.impl;
 
-import com.querydsl.core.Tuple;
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.JPQLQuery;
-import kr.or.kosa.ubun2_be.domain.address.dto.AddressResponseDto;
-import kr.or.kosa.ubun2_be.domain.address.dto.AddressMemberInfoResponse;
-import kr.or.kosa.ubun2_be.domain.address.dto.AddressResponse;
+import com.querydsl.core.QueryResults;
+import kr.or.kosa.ubun2_be.domain.address.entity.Address;
 import kr.or.kosa.ubun2_be.domain.address.entity.QAddress;
 import kr.or.kosa.ubun2_be.domain.address.repository.AddressRepositoryCustom;
 import org.springframework.data.domain.Page;
@@ -14,12 +9,14 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
-import kr.or.kosa.ubun2_be.domain.address.entity.Address;
 
 import java.util.List;
+import java.util.Optional;
 
 import static kr.or.kosa.ubun2_be.domain.address.entity.QAddress.address1;
+import static kr.or.kosa.ubun2_be.domain.customer.entity.QCustomer.customer;
 import static kr.or.kosa.ubun2_be.domain.member.entity.QMember.member;
+import static kr.or.kosa.ubun2_be.domain.member.entity.QMemberCustomer.memberCustomer;
 
 
 @Repository
@@ -29,60 +26,30 @@ public class AddressRepositoryImpl extends QuerydslRepositorySupport implements 
         super(Address.class);
     }
     @Override
-    public Page<AddressResponse> findAllAddressesWithMember(Pageable pageable) {
+    public Page<Address> findAllAddressesWithMember(Pageable pageable, Long customerId) {
         QAddress address = new QAddress("address");
-        JPQLQuery<AddressResponse> query = from(address)
+
+        QueryResults<Address> results = from(address)
                 .join(address.member, member)
-                .select(Projections.constructor(AddressResponse.class,
-                        address.addressId,
-                        member.memberEmail,
-                        member.memberName,
-                        address.address,
-                        address.addressNickname,
-                        address.recipientName,
-                        address.recipientPhone,
-                        address.defaultStatus));
+                .join(member.memberCustomers, memberCustomer)
+                .where(memberCustomer.customer.customerId.eq(customerId)).fetchResults();
 
-        long total = query.fetchCount();
-
-        List<AddressResponse> content = getQuerydsl().applyPagination(pageable, query).fetch();
+        long total = results.getTotal();
+        List<Address> content = results.getResults();
 
         return new PageImpl<>(content, pageable, total);
     }
-    @Override
-    public AddressMemberInfoResponse findMemberInfoByAddressId(Long addressId) {
 
-        Tuple memberInfo = from(address1)
+    @Override
+    public Optional<Address> findAddressByIdAndCustomerId(Long addressId, Long customerId) {
+        Address result = from(address1)
                 .join(address1.member, member)
-                .where(address1.addressId.eq(addressId))
-                .select(member.memberName, member.memberPhone, member.memberEmail, member.createdAt)
+                .join(member.memberCustomers, memberCustomer)
+                .join(memberCustomer.customer, customer)
+                .where(address1.addressId.eq(addressId)
+                        .and(customer.customerId.eq(customerId)))
                 .fetchOne();
 
-        if (memberInfo == null) {
-            return null;
-        }
-
-        // 해당 회원의 모든 주소 목록 조회
-        List<AddressResponseDto> addresses = from(address1)
-                .where(address1.member.memberId.eq(
-                        JPAExpressions.select(address1.member.memberId)
-                                .from(address1)
-                                .where(address1.addressId.eq(addressId))
-                ))
-                .select(Projections.constructor(AddressResponseDto.class,
-                        address1.addressId,
-                        address1.address))
-                .fetch();
-
-
-        // 결과 조합
-        return AddressMemberInfoResponse.builder()
-                .memberName(memberInfo.get(member.memberName))
-                .memberPhone(memberInfo.get(member.memberPhone))
-                .memberEmail(memberInfo.get(member.memberEmail))
-                .registrationDate(memberInfo.get(member.createdAt))
-                .addresses(addresses)
-                .build();
-
+        return Optional.ofNullable(result);
     }
 }
