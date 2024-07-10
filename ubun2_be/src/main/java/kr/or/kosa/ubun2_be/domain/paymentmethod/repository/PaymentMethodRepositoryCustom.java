fd252e0ebfc33adb9c42846a118f149eecc7c2e6
기정김 <404.notfound@kakao.com>
2024-07-10 22:00:47 +0900

    KAN-85 refactor: customerId에 연관관계 확인 로직 추가, querydsl 순서 수정

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/paymentmethod/repository/PaymentMethodRepositoryCustom.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/paymentmethod/repository/PaymentMethodRepositoryCustom.java
@@ -1,15 +1,13 @@
 package kr.or.kosa.ubun2_be.domain.paymentmethod.repository;
 
-import kr.or.kosa.ubun2_be.domain.paymentmethod.dto.AccountPayment.AccountPaymentResponse;
-import kr.or.kosa.ubun2_be.domain.paymentmethod.dto.CardPayment.CardPaymentResponse;
-import kr.or.kosa.ubun2_be.domain.paymentmethod.dto.PaymentMethodDetailResponse;
+import kr.or.kosa.ubun2_be.domain.paymentmethod.entity.PaymentMethod;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface PaymentMethodRepositoryCustom {
-    Page<CardPaymentResponse> findAllCardPaymentMethodsByMemberId(Long memberId, Pageable pageable);
-    Page<AccountPaymentResponse> findAllAccountPaymentMethodsByMemberId(Long memberId, Pageable pageable);
-    Optional<PaymentMethodDetailResponse> findPaymentMethodDetailbyPaymentMethodId(Long paymentMethodId);
+    Page<PaymentMethod> findAllCardPaymentMethodsByMemberId(Long memberId, Pageable pageable, Long customerId);
+    Page<PaymentMethod> findAllAccountPaymentMethodsByMemberId(Long memberId, Pageable pageable, Long customerId);
+    Optional<PaymentMethod> findPaymentMethodbyPaymentMethodIdAndCustomerId(Long paymentMethodId, Long customerId);
 }
