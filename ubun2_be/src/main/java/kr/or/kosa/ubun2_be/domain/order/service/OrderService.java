6a38b0d41e611a5b7cc1a21280898fa430f40469
이소민 <alsth712@naver.com>
2024-07-10 15:59:08 +0900

    KAN-79 feat: 단건 주문 승인 service 추가

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/service/OrderService.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/service/OrderService.java
@@ -1,9 +1,6 @@
 package kr.or.kosa.ubun2_be.domain.order.service;
 
-import kr.or.kosa.ubun2_be.domain.order.dto.OrderDetailResponse;
-import kr.or.kosa.ubun2_be.domain.order.dto.SearchRequest;
-import kr.or.kosa.ubun2_be.domain.order.dto.SubscriptionOrderDetailResponse;
-import kr.or.kosa.ubun2_be.domain.order.dto.UnifiedOrderResponse;
+import kr.or.kosa.ubun2_be.domain.order.dto.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -13,4 +10,10 @@ public interface OrderService {
     OrderDetailResponse getOrderByCustomerIdAndOrderId(Long orderId, Long customerId);
 
     SubscriptionOrderDetailResponse getSubscriptionOrderByCustomerIdAndOrderIdAndCycleNumber(Long orderId, Long customerId, int cycleNumber);
+
+    Page<UnifiedOrderResponse> getPendingOrders(Long customerId, SearchRequest searchRequest, Pageable pageable);
+
+    void updateOrderApprove(Long customerId, OrderApproveRequest orderApproveRequest);
+
+    void updateSubscriptionOrderApprove(Long customerId, SubscriptionApproveRequest subscriptionApproveRequest);
 }
