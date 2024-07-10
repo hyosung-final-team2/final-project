5c5b16f1b2a10edf27e617a1b84a13e06160dbe9
윤동훈 <acttoze1@gmail.com>
2024-07-10 19:26:05 +0900

    KAN-84 feat: 상세조회 데이터 방식 param으로 변경

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/customer/dto/request/MemberDetailRequest.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/customer/dto/request/MemberDetailRequest.java
@@ -1,8 +1,10 @@
 package kr.or.kosa.ubun2_be.domain.customer.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class MemberDetailRequest {
     private Boolean isPending;
 }
