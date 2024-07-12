776507aa4617958eabdc9687291012467d841367
기정김 <404.notfound@kakao.com>
2024-07-12 21:45:49 +0900

    KAN-86 : 주소지 등록 api 연동

--- ubun2_fe/src/customer/components/MemberList/MemberInsertModal/AddressInfoSection/MemberAddressTable.jsx
+++ ubun2_fe/src/customer/components/MemberList/MemberInsertModal/AddressInfoSection/MemberAddressTable.jsx
@@ -1,26 +1,25 @@
 import { Table } from 'flowbite-react';
 
 import MemberAddressTableRow from './MemberAddressTableRow';
-import {tableColumn} from "../../../common/Table/tableIndex.js";
-import TableBody from "../../../common/Table/TableBody.jsx";
-import TableHead from "../../../common/Table/TableHead.jsx";
-import {useEffect, useState} from "react";
-import TablePagination from "../../../common/Pagination/TablePagination.jsx";
+import { tableColumn } from '../../../common/Table/tableIndex.js';
+import TableBody from '../../../common/Table/TableBody.jsx';
+import TableHead from '../../../common/Table/TableHead.jsx';
+import { useEffect, useState } from 'react';
+import TablePagination from '../../../common/Pagination/TablePagination.jsx';
 
-const MemberAddressTable = ({ memberAddresses, title, handleAddressDelete }) => {
+const MemberAddressTable = ({ memberAddresses, title }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [paginatedData, setPaginatedData] = useState([]);
+  const itemsPerPage = 3;
+  const totalPages = Math.ceil(memberAddresses.length / itemsPerPage);
 
-    console.log(memberAddresses);
+  console.log(memberAddresses);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [paginatedData, setPaginatedData] = useState([]);
-    const itemsPerPage = 3;
-    const totalPages = Math.ceil(memberAddresses.length / itemsPerPage);
-
-    useEffect(() => {
-        const startIndex = (currentPage - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        const currentData = memberAddresses.slice(startIndex, endIndex).map((item, index) => {
-            const [addressNum, addressFirst, addressSecond, addressThird] = item.address.split(',');
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentData = memberAddresses.slice(startIndex, endIndex).map((item, index) => {
+      const [addressNum, addressFirst, addressSecond, addressThird] = item.address.split(',');
 
             return {
                 addressId: item.addressId,
@@ -33,18 +32,16 @@ const MemberAddressTable = ({ memberAddresses, title, handleAddressDelete }) => 
             };
         });
         setPaginatedData(currentData);
-        console.log(currentData)
     }, [currentPage, memberAddresses]);
 
-
-    return (
+  return (
     <div className='p-3'>
       <h2 className='text-2xl font-bold mb-3 text-main'>{title}</h2>
       <Table hoverable>
         <TableHead tableColumns={tableColumn.address.detail} isCheckable={false} />
         <TableBody dataList={paginatedData} TableRowComponent={MemberAddressTableRow} isCheckable={false} dynamicId="addressId" handleDelete={handleAddressDelete}/>
       </Table>
-        {totalPages < 1 ? null : <TablePagination totalPages={totalPages} currentPage={currentPage} setCurrentPage={setCurrentPage} />}
+      {totalPages < 1 ? null : <TablePagination totalPages={totalPages} currentPage={currentPage} setCurrentPage={setCurrentPage} />}
     </div>
   );
 };
