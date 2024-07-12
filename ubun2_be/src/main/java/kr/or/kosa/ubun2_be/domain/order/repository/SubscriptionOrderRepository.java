454d5ff12c6cd1aaa95faa84d473b0a275e41bf7
이소민 <alsth712@naver.com>
2024-07-12 19:21:16 +0900

    KAN-90 refactor: 기존 JPQL문 querydsl 변경

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/repository/SubscriptionOrderRepository.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/repository/SubscriptionOrderRepository.java
@@ -9,15 +9,5 @@ import java.util.Optional;
 
 public interface SubscriptionOrderRepository extends JpaRepository<SubscriptionOrder, Long>, SubscriptionOrderRepositoryCustom {
 
-    @Query("SELECT so FROM SubscriptionOrder so " +
-            "JOIN FETCH so.member m " +
-            "JOIN FETCH so.paymentMethod pm " +
-            "JOIN FETCH so.address a " +
-            "JOIN FETCH so.subscriptionOrderProducts sop " +  // Fetch associated products
-            "JOIN m.memberCustomers mc " +
-            "JOIN mc.customer c " +
-            "WHERE so.subscriptionOrderId = :orderId AND c.customerId = :customerId " +
-            "AND sop.cycleNumber = :cycleNumber")
-    Optional<SubscriptionOrder> findSubscriptionOrderByIdAndCustomerIdAndCycleNumber(@Param("orderId") Long orderId, @Param("customerId") Long customerId, @Param("cycleNumber") int cycleNumber);
 }
 
