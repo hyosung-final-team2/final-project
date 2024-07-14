ef8df8676db818a92d9d349b868d832b5f50b8bb
정혜미 <113500975+hamings@users.noreply.github.com>
2024-07-14 21:19:14 +0900

    KAN-130 feat: categoryResponse dto 구현

---
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/dto/CategoryResponse.java
@@ -0,0 +1,15 @@
+package kr.or.kosa.ubun2_be.domain.product.dto;
+
+import kr.or.kosa.ubun2_be.domain.product.entity.Category;
+import lombok.Getter;
+
+@Getter
+public class CategoryResponse { //카테고리 조회, 속성 생각해보기
+    private Long categoryId;
+    private String categoryName;
+
+    public CategoryResponse(Category category) {
+        this.categoryId = category.getCategoryId();
+        this.categoryName = category.getCategoryName();
+    }
+}
