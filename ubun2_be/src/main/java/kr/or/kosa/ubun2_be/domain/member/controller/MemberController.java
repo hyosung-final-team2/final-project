f1b3e60796b95c472f6ff210af82978ef84971a7
정혜미 <113500975+hamings@users.noreply.github.com>
2024-07-14 17:02:07 +0900

    KAN-103 feat: 등록된 상점 목록 조회 기능 구현

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/member/controller/MemberController.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/member/controller/MemberController.java
@@ -1,11 +1,16 @@
 package kr.or.kosa.ubun2_be.domain.member.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import kr.or.kosa.ubun2_be.domain.member.dto.CustomerResponse;
 import kr.or.kosa.ubun2_be.domain.member.dto.MemberSignUpRequest;
 import kr.or.kosa.ubun2_be.domain.member.service.MemberService;
+import kr.or.kosa.ubun2_be.global.auth.model.CustomUserDetails;
 import kr.or.kosa.ubun2_be.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +23,13 @@ public class MemberController {
     public ResponseDto<?> createMember(@RequestBody MemberSignUpRequest memberSignUpRequest) {
         memberService.createMember(memberSignUpRequest);
         return ResponseDto.ok(null, "정상출력 데이터");
+    }
+
+    @Operation(summary = "등록 상점 조회")
+    @GetMapping("/stores")
+    public ResponseDto<?> getStores(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        List<CustomerResponse> customers = memberService.getCustomers(customUserDetails.getUserId());
+        return ResponseDto.ok(customers, "정상출력 데이터");
     }
 
 }
--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/member/service/MemberService.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/member/service/MemberService.java
@@ -1,7 +1,13 @@
 package kr.or.kosa.ubun2_be.domain.member.service;
 
+import kr.or.kosa.ubun2_be.domain.member.dto.CustomerResponse;
 import kr.or.kosa.ubun2_be.domain.member.dto.MemberSignUpRequest;
+
+import java.util.List;
 
 public interface MemberService {
     void createMember(MemberSignUpRequest memberSignUpRequest);
+
+    List<CustomerResponse> getCustomers(Long memberId);
+
 }
