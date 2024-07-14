ce15b3fb8db550abf1f00aeb2fa8fed2f888400d
정혜미 <113500975+hamings@users.noreply.github.com>
2024-07-14 21:20:07 +0900

    KAN-130 feat: 카테고리 목록 조회 기능 구현

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/repository/CategoryRepository.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/repository/CategoryRepository.java
@@ -1,10 +1,16 @@
 package kr.or.kosa.ubun2_be.domain.product.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import kr.or.kosa.ubun2_be.domain.product.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByCategoryName(String categoryName);
+
+    @Query("select distinct c from Category c join c.products p where p.customer.customerId = :customerId")
+    List<Category> findDistinctByCustomerId(@Param("customerId") Long customerId);
 }
--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/service/CategoryService.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/product/service/CategoryService.java
@@ -1,7 +1,11 @@
 package kr.or.kosa.ubun2_be.domain.product.service;
 
+import kr.or.kosa.ubun2_be.domain.product.dto.CategoryResponse;
 import kr.or.kosa.ubun2_be.domain.product.entity.Category;
+
+import java.util.List;
 
 public interface CategoryService {
     Category findCategoryByCategoryName(String categoryName);
+    List<CategoryResponse> getCategories(Long customerId, Long memberId);
 }
