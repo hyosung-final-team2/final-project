d59bf7243954a4d4cb83a5ecffb7bdf31800b68c
김기정 <59950506+epass1123@users.noreply.github.com>
2024-07-15 09:32:14 +0900

    KAN-109 chore: 함수 변경 & props 추가

--- ubun2_fe/src/member/components/common/button/BottomButton.jsx
+++ ubun2_fe/src/member/components/common/button/BottomButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function BottomButton({ buttonText, buttonStyle }) {
+const BottomButton = ({ buttonText, buttonStyle, setIsModalOpen }) => {
   return (
     <button onClick={() => setIsModalOpen(false)} className={`${buttonStyle} font-bold w-full h-14 rounded-2xl mt-5`}>
       {buttonText}
     </button>
   );
-}
+};
 
 export default BottomButton;
