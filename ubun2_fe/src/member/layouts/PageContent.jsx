5f6d90786e0ea77be4091d937ba6eb2114b9a545
김기정 <59950506+epass1123@users.noreply.github.com>
2024-07-15 17:45:19 +0900

    KAN-109 chore: 임시라우팅 연결

--- ubun2_fe/src/member/layouts/PageContent.jsx
+++ ubun2_fe/src/member/layouts/PageContent.jsx
@@ -8,6 +8,10 @@ import StoreList from '../pages/internal/StoreList';
 import Cart from '../pages/internal/Cart';
 import Store from '../pages/internal/Store';
 import Product from '../pages/internal/Product';
+import ChooseAddress from '../pages/internal/ChooseAddress';
+import RegisterAddress from '../pages/internal/RegisterAddress';
+import AddressSearchMobilePopUp from '../components/Address/AddressSearchMobilePopUp';
+import ChoosePayment from '../pages/internal/ChoosePayment';
 
 function PageContent({ hasFootNav }) {
   const { memberId } = useMemberStore(state => ({ memberId: state.memberId }));
@@ -20,8 +24,12 @@ function PageContent({ hasFootNav }) {
           <Route path='mypage' element={<MyPage />} />
           <Route path='home' element={<StoreList />} />
           <Route path='cart' element={<Cart />} />
+          <Route path='addresses' element={<ChooseAddress />} />
+          <Route path='addresses/register' element={<RegisterAddress />} />
           <Route path='store/:customerId' element={<Store />} />
           <Route path='store/:customerId/product/:productId' element={<Product />} />
+          <Route path='addresses/address-search' element={<AddressSearchMobilePopUp />} />
+          <Route path='payments' element={<ChoosePayment />} />
         </Routes>
       </main>
     </div>
