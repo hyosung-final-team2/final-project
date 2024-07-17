abf066d71cf9313dd0773c28c26fc95f060bc87a
이소민 <alsth712@naver.com>
2024-07-17 22:16:58 +0900

    Kan-123 feat : 마이페이지 라우팅 추가

--- ubun2_fe/src/member/layouts/PageContent.jsx
+++ ubun2_fe/src/member/layouts/PageContent.jsx
@@ -15,7 +15,7 @@ import ChoosePayment from '../pages/internal/ChoosePayment';
 import EditPayment from '../pages/internal/EditPayment';
 import MyPaymentsList from '../pages/internal/MyPaymentsList';
 import Order from '../pages/internal/Order';
-import InputPassword from '../pages/internal/InputPassword';
+// import InputPassword from '../pages/internal/InputPassword';
 import OrderComplete from '../pages/internal/OrderComplete';
 
 function PageContent({ hasFootNav }) {
@@ -37,7 +37,7 @@ function PageContent({ hasFootNav }) {
           <Route path='payments' element={<ChoosePayment />} />
           <Route path='payments/edit' element={<EditPayment />} />
           <Route path='mypage/payment-list' element={<MyPaymentsList />} />
-          <Route path='password' element={<InputPassword />} />
+          {/* <Route path='password' element={<InputPassword />} /> */}
           <Route path='order/:orderId' element={<Order />} />
           <Route path='order-complete/:orderId' element={<OrderComplete />} />
         </Routes>
