74a481385ce6eb3ec7016ace1a12a78045b65947
이소민 <alsth712@naver.com>
2024-07-18 10:47:23 +0900

    KAN-125 feat: 라우팅 추가

--- ubun2_fe/src/member/layouts/PageContent.jsx
+++ ubun2_fe/src/member/layouts/PageContent.jsx
@@ -15,10 +15,10 @@ import ChoosePayment from '../pages/internal/ChoosePayment';
 import EditPayment from '../pages/internal/EditPayment';
 import MyPaymentsList from '../pages/internal/MyPaymentsList';
 import Order from '../pages/internal/Order';
-// import InputPassword from '../pages/internal/InputPassword';
+import InputPassword from '../pages/internal/InputPassword';
 import OrderComplete from '../pages/internal/OrderComplete';
 import MyOrdersList from '../pages/internal/MyOrdersList';
-
+import MySingleOrderDetail from '../pages/internal/MySingleOrderDetail';
 
 function PageContent({ hasFootNav }) {
   const { memberId } = useMemberStore(state => ({ memberId: state.memberId }));
@@ -39,10 +39,11 @@ function PageContent({ hasFootNav }) {
           <Route path='payments' element={<ChoosePayment />} />
           <Route path='payments/edit' element={<EditPayment />} />
           <Route path='mypage/payment-list' element={<MyPaymentsList />} />
-          {/*<Route path='password' element={<InputPassword />} />*/}
+          <Route path='password' element={<InputPassword />} />
           <Route path='order/:orderId' element={<Order />} />
           <Route path='order-complete/:orderId' element={<OrderComplete />} />
           <Route path='mypage/order-list' element={<MyOrdersList />} />
+          <Route path='mypage/single-order/:customerId/:orderId' element={<MySingleOrderDetail />} />
         </Routes>
       </main>
     </div>
