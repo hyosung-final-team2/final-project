4831910c447be8486d07676677c528274deba0ff
이소민 <alsth712@naver.com>
2024-07-20 15:31:17 +0900

    KAN-162 chore: 피드백 반영

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/dto/CancelOrderRequest.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/order/dto/CancelOrderRequest.java
@@ -1,10 +1,8 @@
 package kr.or.kosa.ubun2_be.domain.order.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class CancelOrderRequest {
     private Long customerId;
     private Long orderId;
