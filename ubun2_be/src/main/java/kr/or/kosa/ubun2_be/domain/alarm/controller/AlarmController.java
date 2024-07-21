9fdd768884b45022f2dbc910aa0c4f68ecd567ab
윤동훈 <acttoze1@gmail.com>
2024-07-21 22:41:13 +0900

    KAN-182 chore: config 변경

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/alarm/controller/AlarmController.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/alarm/controller/AlarmController.java
@@ -13,35 +13,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/alarm")
 @RequiredArgsConstructor
 public class AlarmController {
 
     private final AlarmService alarmService;
 
     @Operation(summary = "단일 구매자에게 알람 보내기")
-    @PostMapping("/personal")
+    @PostMapping("/customers/alarm/personal")
     public ResponseDto<?> pushMessagePersonal(@RequestBody PersonalAlarmSendRequest request) {
         String messageId = alarmService.sendMessageToPersonal(request);
         return ResponseDto.ok(messageId,"개인 알람 전송 성공");
     }
 
     @Operation(summary = "고객의 상점에 구독되어있는 회원 전체에게 알람 보내기")
-    @PostMapping("/group")
+    @PostMapping("/customers/alarm/group")
     public ResponseDto<?> pushMessageGroup(@RequestBody GroupAlarmSendRequest request) {
         alarmService.sendMessageToGroup(request);
         return ResponseDto.ok(null,"토픽 알람 전송 성공");
     }
 
     @Operation(summary = "고객별 알림 조회")
-    @GetMapping("/{memberId}")
+    @GetMapping("/members/alarm/{memberId}")
     public ResponseDto<List<Alarm>> getPushMessages(@PathVariable Long memberId) {
         List<Alarm> alarms = alarmService.getPushMessages(memberId);
         return ResponseDto.ok(alarms, "메시지 조회 성공");
     }
 
     @Operation(summary = "알림 읽음 처리")
-    @DeleteMapping("/{memberId}/{alarmId}")
+    @DeleteMapping("/members/alarm/{memberId}/{alarmId}")
     public ResponseDto<?> markAlarmAsRead(@PathVariable Long memberId, @PathVariable String alarmId) {
         alarmService.markAsRead(memberId, alarmId);
         return ResponseDto.ok(null, "알림 읽음 처리 완료");
