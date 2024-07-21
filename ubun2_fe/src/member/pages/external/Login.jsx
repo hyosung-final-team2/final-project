bf2d62fa0ce4a93e556f243bf7cfe19a6b155a16
이소민 <alsth712@naver.com>
2024-07-21 20:18:03 +0900

    KAN-173 chore: 로그인 시, 카트 선택지 초기화

--- ubun2_fe/src/member/pages/external/Login.jsx
+++ ubun2_fe/src/member/pages/external/Login.jsx
@@ -1,21 +1,22 @@
-import InputText from "../../../customer/components/common/Input/InputText.jsx";
-import {useState} from "react";
-import {useLocation, useNavigate} from "react-router-dom";
-import {useLogin} from "../../../customer/api/common/Login/queris.js";
-import CustomButton from "../../components/common/button/CustomButton.jsx";
-import InstallPrompt from "../../components/Installprompt/InstallPrompt.jsx";
+import InputText from '../../../customer/components/common/Input/InputText.jsx';
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useLogin } from '../../../customer/api/common/Login/queris.js';
+import CustomButton from '../../components/common/button/CustomButton.jsx';
+import InstallPrompt from '../../components/Installprompt/InstallPrompt.jsx';
+import useOrderItemsStore from '../../store/order/orderItemStore.js';
 
 const Login = () => {
-
   const INITIAL_LOGIN_OBJ = {
     password: '',
     loginId: '',
   };
 
-  const [loginObj, setLoginObj] = useState(INITIAL_LOGIN_OBJ)
+  const [loginObj, setLoginObj] = useState(INITIAL_LOGIN_OBJ);
   const [errorMessage, setErrorMessage] = useState('');
   const role = useLocation().pathname === '/customer/login' ? 'ROLE_CUSTOMER' : 'ROLE_MEMBER';
   const navigate = useNavigate();
+  const { clearCart } = useOrderItemsStore();
 
   const updateFormValue = ({ updateType, value }) => {
     setErrorMessage('');
@@ -30,51 +31,60 @@ const Login = () => {
     if (loginObj.loginId.trim() === '') return setErrorMessage('Email Id is required! (use any value)');
     if (loginObj.password.trim() === '') return setErrorMessage('Password is required! (use any value)');
     else {
-      loginMutate({}, {
-        onSuccess: () => {
-          navigate("/member/app/home")
-        },
-        onError: () => console.log("로그인 실패")
-      });
-
+      loginMutate(
+        {},
+        {
+          onSuccess: () => {
+            clearCart();
+            navigate('/member/app/home');
+          },
+          onError: () => console.log('로그인 실패'),
+        }
+      );
     }
   };
 
   const signupButtonFunc = () => {
-    navigate("/member/register")
-  }
+    navigate('/member/register');
+  };
 
   const fotgotInfoFunc = () => {
-    navigate("/member/forgot-loginid")
-  }
+    navigate('/member/forgot-loginid');
+  };
 
-  const buttonStyle="bg-main text-white mt-4";
-  const secondButtonStyle = "bg-second text-main mt-2"
+  const buttonStyle = 'bg-main text-white mt-4';
+  const secondButtonStyle = 'bg-second text-main mt-2';
 
   return (
-    <div className='mobile_container p-4'>
-      <h1 className='text-3xl text-main text-center font-bold'>로그인</h1>
-        <InputText defaultValue={loginObj.loginId}
-                   updateType='loginId'
-                   containerStyle='mt-4'
-                   updateFormValue={updateFormValue}
-                   placeholder='아이디'
-                   isError={isError}/>
-        <InputText defaultValue={loginObj.password}
-                   updateType='password'
-                   type="password"
-                   containerStyle='mt-4'
-                   updateFormValue={updateFormValue}
-                   placeholder='비밀번호'
-                   isError={isError}/>
+    <div className='p-4 mobile_container'>
+      <h1 className='text-3xl font-bold text-center text-main'>로그인</h1>
+      <InputText
+        defaultValue={loginObj.loginId}
+        updateType='loginId'
+        containerStyle='mt-4'
+        updateFormValue={updateFormValue}
+        placeholder='아이디'
+        isError={isError}
+      />
+      <InputText
+        defaultValue={loginObj.password}
+        updateType='password'
+        type='password'
+        containerStyle='mt-4'
+        updateFormValue={updateFormValue}
+        placeholder='비밀번호'
+        isError={isError}
+      />
 
-      <CustomButton buttonStyle={buttonStyle} buttonText="로그인" buttonFunc={submitForm}/>
-      <CustomButton buttonStyle={secondButtonStyle} buttonText="회원가입" buttonFunc={signupButtonFunc}/>
-      <p onClick={() => fotgotInfoFunc()} className='mt-2 text-gray-400 underline text-end'>계정 정보를 잊어버렸어요</p>
+      <CustomButton buttonStyle={buttonStyle} buttonText='로그인' buttonFunc={submitForm} />
+      <CustomButton buttonStyle={secondButtonStyle} buttonText='회원가입' buttonFunc={signupButtonFunc} />
+      <p onClick={() => fotgotInfoFunc()} className='mt-2 text-gray-400 underline text-end'>
+        계정 정보를 잊어버렸어요
+      </p>
 
-      <InstallPrompt/>
+      <InstallPrompt />
     </div>
   );
-}
+};
 
 export default Login;
