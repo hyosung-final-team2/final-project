7c4f6891065173fb2ebf0f438e3ca3ff1bffa65a
정혜미 <113500975+hamings@users.noreply.github.com>
2024-07-22 22:30:21 +0900

    KAN-189 refactor:config 수정

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/global/config/RedisConfig.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/global/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package kr.or.kosa.ubun2_be.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,9 +11,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(host,port);
     }
 
     @Bean
