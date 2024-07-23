ab6fc8416b4cd55c2b94aa608d53c0514324d523
이소민 <alsth712@naver.com>
2024-07-23 12:46:16 +0900

    KAN-175 feat: 주문 API 연동

--- ubun2_fe/src/member/api/Order/queris.js
+++ ubun2_fe/src/member/api/Order/queris.js
@@ -1,8 +1,43 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { createOrder } from './order';
+import { createOrder, getOrderDetail, getOrderList, getSubscriptionOrder, updateOrderCancel, updateSubscriptionCancel } from './order';
 
 export const useCreateOrder = () => {
   return useMutation({
     mutationFn: data => createOrder(data),
+  });
+};
+
+export const useGetOrderList = () => {
+  return useQuery({
+    queryKey: ['orders'],
+    queryFn: () => getOrderList(),
+  });
+};
+
+export const useGetOrderDetail = (customerId, orderId) => {
+  return useQuery({
+    queryKey: ['orders', customerId, orderId],
+    queryFn: () => getOrderDetail(customerId, orderId),
+    enabled: !!customerId && !!orderId,
+  });
+};
+
+export const useGetSubscriptionOrderDetail = (customerId, orderId) => {
+  return useQuery({
+    queryKey: ['subscriptionOrder', customerId, orderId],
+    queryFn: () => getSubscriptionOrder(customerId, orderId),
+    enabled: !!customerId && !!orderId,
+  });
+};
+
+export const useUpdateCancelOrder = () => {
+  return useMutation({
+    mutationFn: data => updateOrderCancel(data), // TODO: invalidateQueries 적용하기
+  });
+};
+
+export const useUpdateSuscriptionCancelOrder = () => {
+  return useMutation({
+    mutationFn: data => updateSubscriptionCancel(data), // TODO: invalidateQueries 적용하기
   });
 };
