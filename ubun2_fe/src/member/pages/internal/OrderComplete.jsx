08b2d97b3ba2ad56860730c23c04d17c1a394179
이소민 <alsth712@naver.com>
2024-07-23 12:53:41 +0900

    KAN-173 feat: 주문 등록 API 수정 및 연동

--- ubun2_fe/src/member/pages/internal/OrderComplete.jsx
+++ ubun2_fe/src/member/pages/internal/OrderComplete.jsx
@@ -1,24 +1,82 @@
 import InformationCircleIcon from '@heroicons/react/24/outline/InformationCircleIcon';
 import ChevronRightIcon from '@heroicons/react/24/solid/ChevronRightIcon';
+import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useGetMyAddresses } from '../../api/Address/queries';
+import { useCreateOrder } from '../../api/Order/queris';
+import { useGetAccount, useGetCard } from '../../api/Payment/queries';
 import BottomButton from '../../components/common/button/BottomButton';
+import DoubleBottomButton from '../../components/common/button/DoubleBottomButton';
 import PaymentSummaryCompleted from '../../components/common/paymentSummary/PaymentSummaryCompleted';
+import SlideUpModal from '../../components/common/SlideUpModal';
 import OrderCompleteStore from '../../components/OrderComplete/OrderCompleteStore';
+import useModalStore from '../../store/modalStore';
+import useOrderDataStore from '../../store/order/orderDataStore';
 import useOrderItemsStore from '../../store/order/orderItemStore';
 
 const OrderComplete = () => {
-  const { selectedItems, calculateTotals } = useOrderItemsStore();
+  const { selectedItems, calculateTotals, clearCart } = useOrderItemsStore();
+  const { orderData, selectedAddressId, selectedPaymentMethodId, selectedPaymentMethodType, resetOrderData } = useOrderDataStore();
   const navigate = useNavigate();
-
+  const createOrderMutation = useCreateOrder();
+  const { data: addresses, isLoading: addressesLoading, isError: addressesError } = useGetMyAddresses();
+  const { data: card, isLoading: cardLoading, isError: cardError } = useGetCard(selectedPaymentMethodId);
+  const { data: account, isLoading: accountLoading, isError: accountError } = useGetAccount(selectedPaymentMethodId);
+  const addressInfo = addresses?.data?.data.find(address => address.addressId === selectedAddressId);
+  const { modalState, setModalState } = useModalStore();
   const totals = calculateTotals();
-  const paymentInfo = {
-    paymentName: selectedItems.paymentType === 'CARD' ? selectedItems.cardName : selectedItems.accountName,
-    paymentContent: selectedItems.paymentType === 'CARD' ? selectedItems.cardNumber : selectedItems.accountNumber,
-  };
+  const modalButtonStyle = 'bg-main text-white';
 
-  const buttonFunc = () => {
+  const paymentInfo =
+    selectedPaymentMethodType === 'CARD'
+      ? {
+          paymentName: card?.cardName,
+          paymentContent: card?.cardNumber,
+        }
+      : {
+          paymentName: account?.accountName,
+          paymentContent: account?.accountNumber,
+        };
+
+  const handleConfirmOrder = useCallback(async () => {
+    const orderDataWithPayment = orderData.map(item => ({
+      ...item,
+      addressId: selectedAddressId,
+    }));
+
+    try {
+      console.log('주문 데이터:', orderDataWithPayment);
+      await createOrderMutation.mutateAsync(orderDataWithPayment);
+      console.log('주문이 완료되었습니다.');
+      // 주문 완료 후 데이터 초기화
+      resetOrderData();
+      clearCart();
+      navigate('/member/app/home');
+    } catch (error) {
+      console.log('주문에 실패했습니다:', error);
+    }
+  }, [orderData, selectedAddressId, createOrderMutation, navigate]);
+
+  const handleCancel = () => {
+    resetOrderData();
     navigate('/member/app/home');
   };
+
+  const handleCloseModal = () => {
+    setModalState(false);
+  };
+
+  const openCancelModal = () => {
+    setModalState(true);
+  };
+
+  if (addressesLoading || cardLoading || accountLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (addressesError || cardError || accountError) {
+    return <div>Error loading data</div>;
+  }
 
   return (
     <div>
@@ -26,46 +84,42 @@ const OrderComplete = () => {
         <h1 className='mb-2 text-3xl font-bold'>결제가 완료됐어요</h1>
         <div className='flex gap-3 py-4'>
           <span>주문 일자 </span>
-          <span className='text-gray-500'>{`${selectedItems.createdAt}`}</span>
+          <span className='text-gray-500'>{new Date().toLocaleDateString()}</span>
         </div>
       </div>
 
-      {selectedItems.itemContent.map(store => (
+      {selectedItems.map(store => (
         <div key={`${store.customerId}-${store.businessName}`}>
-          {store.singleOrderProducts.length > 0 && (
+          {store.cartProducts.length > 0 && (
             <OrderCompleteStore
               store={{
                 ...store,
                 orderType: '단건 주문',
-                products: store.singleOrderProducts,
-              }}
-            />
-          )}
-          {store.regularOrderProducts.length > 0 && (
-            <OrderCompleteStore
-              store={{
-                ...store,
-                orderType: '정기 주문',
-                products: store.regularOrderProducts,
+                products: store.cartProducts.map(product => ({
+                  ...product,
+                  ...orderData
+                    .find(order => order.customerId === store.customerId)
+                    ?.subscriptionOrderProducts.find(subProduct => subProduct.productId === product.productId),
+                })),
               }}
             />
           )}
         </div>
       ))}
 
-      <div className='p-4 py-8 mb-4 bg-white'>
+      <div className='flex flex-col gap-2 p-4 py-8 mb-4 bg-white'>
         <h2 className='mb-2 text-xl font-semibold'>배송지 정보</h2>
-        <p className='font-semibold'>{selectedItems.addressNickname}</p>
-        <p className='text-sm text-gray-600'>{selectedItems.address}</p>
-        <p className='text-sm text-gray-600'>{selectedItems.phoneNumber}</p>
-        <div>
+        <p className='font-semibold'>{addressInfo?.addressNickname}</p>
+        <p className='text-sm text-gray-600'>{addressInfo?.address}</p>
+        <p className='text-sm text-gray-600'>{addressInfo?.phoneNumber}</p>
+        <div className='flex gap-5 '>
           <InformationCircleIcon className='w-5' />
-          <p className='mt-2 text-sm text-gray-500'>배송지 변경을 원하시면 결제취소 후 다시 주문해주세요</p>
+          <p className='text-sm text-gray-500'>배송지 변경을 원하시면 결제취소 후 다시 주문해주세요</p>
         </div>
       </div>
 
       <div className='w-full py-2 bg-white'>
-        <div className='flex justify-between p-4'>
+        <div className='flex justify-between p-4' onClick={openCancelModal}>
           <span className='text-lg font-bold text-red-700'>결제 취소하기</span>
           <ChevronRightIcon className='w-5' />
         </div>
@@ -85,9 +139,21 @@ const OrderComplete = () => {
 
       <PaymentSummaryCompleted productAmount={totals.productAmount} discount={totals.discount} totalAmount={totals.totalAmount} paymentInfo={paymentInfo} />
 
-      <div className='sticky bottom-0 left-0 right-0 p-4 bg-white'>
-        <BottomButton buttonText='확인했어요' buttonStyle='bg-main text-white' buttonFunc={buttonFunc} />
+      <div
+        className='sticky bottom-0 left-0 right-0 flex flex-col w-full p-4 px-3 py-4'
+        style={{ background: 'linear-gradient(to top, white, white 65%, transparent)' }}
+      >
+        <BottomButton buttonText='확인했어요' buttonStyle='bg-main text-white' buttonFunc={handleConfirmOrder} />
       </div>
+      <SlideUpModal isOpen={modalState} setIsModalOpen={setModalState} headerText='주문을 취소하시겠습니까?' isButton={false}>
+        <DoubleBottomButton
+          buttonStyle={modalButtonStyle}
+          firstButtonText='취소'
+          secondButtonText='삭제'
+          firstButtonFunc={handleCloseModal}
+          secondButtonFunc={handleCancel}
+        />
+      </SlideUpModal>
     </div>
   );
 };
