a37fe35a9ca6e70073c18b9ac70c19a032944ca0
이소민 <alsth712@naver.com>
2024-07-26 10:45:33 +0900

    KAN-205 chore: null값 처리

--- ubun2_fe/src/customer/utils/cardFormat.js
+++ ubun2_fe/src/customer/utils/cardFormat.js
@@ -4,13 +4,13 @@
  * @returns {string}
  */
 export const formatCardNumber = cardNumber => {
-  const cleaned = cardNumber.replace(/\D/g, '');
+  const cleaned = cardNumber?.replace(/\D/g, '');
   const formatted = cleaned.replace(/(\d{4})(?=\d)/g, '$1-');
   return formatted;
 };
 
 export const formatCardNumberWithSpace = cardNumber => {
-  const cleaned = cardNumber.replace(/\D/g, '');
+  const cleaned = cardNumber?.replace(/\D/g, '');
   const formatted = cleaned.replace(/(\d{4})(?=\d)/g, '$1 ');
   return formatted;
 };
@@ -21,7 +21,7 @@ export const formatCardNumberWithSpace = cardNumber => {
  * @returns {string}
  */
 export const maskCardNumber = cardNumber => {
-  const cleaned = cardNumber.replace(/\D/g, '');
+  const cleaned = cardNumber?.replace(/\D/g, '');
   const masked = cleaned.replace(/(\d{6})(\d+)(\d{4})/, '$1-****-****-$3');
   return masked;
 };
