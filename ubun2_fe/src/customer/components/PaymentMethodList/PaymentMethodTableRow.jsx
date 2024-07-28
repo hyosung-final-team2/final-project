c8b7e1b280b35ff546ace37b1a299b2093c4a439
김기정 <59950506+epass1123@users.noreply.github.com>
2024-07-28 22:09:39 +0900

    KAN-200 feat: PaymentMethod table skeleton 추가

--- ubun2_fe/src/customer/components/PaymentMethodList/PaymentMethodTableRow.jsx
+++ ubun2_fe/src/customer/components/PaymentMethodList/PaymentMethodTableRow.jsx
@@ -33,14 +33,16 @@ const PaymentMethodTableRow = ({
   return (
     <>
       <Table.Row className='bg-white' onClick={() => setOpenModal(paymentMethodId, memberId, payment)}>
-        <Table.Cell>
+        <Table.Cell style={{ width: '5%' }}>
           <Checkbox checked={isChecked} onChange={() => handleRowChecked(id)} onClick={e => e.stopPropagation()} />
         </Table.Cell>
-        <Table.Cell>{memberEmail}</Table.Cell>
-        <Table.Cell>{memberName}</Table.Cell>
-        <Table.Cell>{isAccount ? <PaymentMethodBadge paymentType={paymentMethodType} /> : <PaymentMethodBadge paymentType={paymentMethodType} />}</Table.Cell>
-        <Table.Cell>{isAccount ? bankName : cardCompanyName}</Table.Cell>
-        <Table.Cell>{isAccount ? maskAccountNumber(accountNumber) : maskCardNumber(cardNumber)}</Table.Cell>
+        <Table.Cell style={{ width: '25%' }}>{memberEmail}</Table.Cell>
+        <Table.Cell style={{ width: '15%' }}>{memberName}</Table.Cell>
+        <Table.Cell style={{ width: '15%' }}>
+          {isAccount ? <PaymentMethodBadge paymentType={paymentMethodType} /> : <PaymentMethodBadge paymentType={paymentMethodType} />}
+        </Table.Cell>
+        <Table.Cell style={{ width: '20%' }}>{isAccount ? bankName : cardCompanyName}</Table.Cell>
+        <Table.Cell style={{ width: '20%' }}>{isAccount ? maskAccountNumber(accountNumber) : maskCardNumber(cardNumber)}</Table.Cell>
       </Table.Row>
     </>
   );
