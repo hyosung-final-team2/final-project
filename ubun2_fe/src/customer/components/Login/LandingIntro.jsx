ebfbefb8cc24071272c9b905d37f86a5fb8dfe64
윤동훈 <acttoze1@gmail.com>
2024-07-29 21:07:24 +0900

    KAN-169 style: login-page style

--- ubun2_fe/src/customer/components/Login/LandingIntro.jsx
+++ ubun2_fe/src/customer/components/Login/LandingIntro.jsx
@@ -1,8 +1,22 @@
 const LandingIntro = () => {
   return (
-    <div className='hero min-h-full rounded-l-xl bg-base-200'>
-      <div className='hero-content py-12'>
-        <div className='max-w-md'></div>
+    <div className='hero min-h-full rounded-l-xl main_left'>
+      <div className='hero-content py-12 w-full h-full'>
+        <div className='max-w-md w-full h-full'>
+            <div className="content">
+                <div className="content__container">
+                    <ul className="content__container__list">
+                        <li className="content__container__list__item">고객을</li>
+                        <li className="content__container__list__item">회원을</li>
+                        <li className="content__container__list__item">모두를</li>
+                    </ul>
+                    <div className="content__container__text flex justify-end items-center" style={{}}>
+                        <span className="mr-3">위한</span>
+                        <img src="/text_logo.png" style={{width:"160px"}}/>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   );
--- ubun2_fe/src/customer/pages/external/ForgotPassword.jsx
+++ ubun2_fe/src/customer/pages/external/ForgotPassword.jsx
@@ -10,18 +10,11 @@ const ForgotPassword = () => {
 
     return (
         <>
-            <div className='min-h-screen bg-base-200 flex items-center'>
-                <div className='card mx-auto w-full max-w-5xl  shadow-xl'>
-                    <div className='grid  md:grid-cols-2 grid-cols-1  bg-base-100 rounded-xl'>
-                        <div className=''>
-                            <LandingIntro />
-                        </div>
+
                         <div className='py-24 px-10'>
                             {!isSuccess ? <FindPassword setIsSuccess={setIsSuccess}/> : !isResetSuccess ? <ResetPassword setIsResetSuccess={setIsResetSuccess}/> : <ResetPasswordResult/>}
                         </div>
-                    </div>
-                </div>
-            </div>
+
         </>
     )
 };
