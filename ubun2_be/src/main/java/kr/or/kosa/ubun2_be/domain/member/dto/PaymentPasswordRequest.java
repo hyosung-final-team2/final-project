a5d0d3ddba0ecca157e96b1e0cae560e557274c8
이소민 <alsth712@naver.com>
2024-07-29 21:10:08 +0900

    KAN-105 chore: 주석 처리 제거

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/member/dto/PaymentPasswordRequest.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/member/dto/PaymentPasswordRequest.java
@@ -1,11 +1,13 @@
 package kr.or.kosa.ubun2_be.domain.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class PaymentPasswordRequest {
-//
-//    @NotBlank
-//    @Pattern(regexp = "^\\d{6}$", message = "간편 결제 비밀번호를 입력해주세요")
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{6}$", message = "간편 결제 비밀번호를 입력해주세요")
     private String paymentPassword;
 }
