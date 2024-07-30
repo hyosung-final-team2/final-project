012d73373e75ec27eee269c8102b7a66608c6b9b
윤동훈 <acttoze1@gmail.com>
2024-07-30 17:11:17 +0900

    KAN-232 feat: addressTable 정렬 시 text-bold

--- ubun2_fe/src/customer/components/AddressList/Skeleton/SkeletonAddressTableRow.jsx
+++ ubun2_fe/src/customer/components/AddressList/Skeleton/SkeletonAddressTableRow.jsx
@@ -1,6 +1,18 @@
 import { Checkbox, Table } from 'flowbite-react';
+import useAddressTableStore from "../../../store/Address/addressTableStore.js";
 
 const SkeletonAddressTableRow = ({ memberName, address }) => {
+
+  const { sort } = useAddressTableStore();
+
+  const getColorForColumn = (column) => {
+    const sortItem = sort.find(item => item.startsWith(`${column},`));
+    if (sortItem) {
+      return sortItem.endsWith('DESC') ? 'font-bold' : 'font-bold';
+    }
+    return '';
+  };
+
   if (!address) return;
   const [zipNo, city, town, ...detail] = address?.split(',');
 
@@ -11,11 +23,11 @@ const SkeletonAddressTableRow = ({ memberName, address }) => {
         <Table.Cell style={{ width: '5%' }}>
           <Checkbox />
         </Table.Cell>
-        <Table.Cell style={{ width: '15%' }}>{memberName}</Table.Cell>
-        <Table.Cell style={{ width: '15%' }}>{city}</Table.Cell>
-        <Table.Cell style={{ width: '15%' }}>{town}</Table.Cell>
-        <Table.Cell style={{ width: '35%' }}>{detail}</Table.Cell>
-        <Table.Cell style={{ width: '15%' }}>{zipNo}</Table.Cell>
+        <Table.Cell className={getColorForColumn('memberName')} style={{ width: '15%' }}>{memberName}</Table.Cell>
+        <Table.Cell className={getColorForColumn('address')} style={{ width: '15%' }}>{city}</Table.Cell>
+        <Table.Cell className={getColorForColumn('address')} style={{ width: '15%' }}>{town}</Table.Cell>
+        <Table.Cell className={getColorForColumn('address')} style={{ width: '35%' }}>{detail}</Table.Cell>
+        <Table.Cell className={getColorForColumn('address')} style={{ width: '15%' }}>{zipNo}</Table.Cell>
       </Table.Row>
     </>
   );
