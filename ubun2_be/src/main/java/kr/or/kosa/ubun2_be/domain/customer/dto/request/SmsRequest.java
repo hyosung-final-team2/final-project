6a5f3090c84ecaa7b3728403139b90d2169ce72d
정혜미 <113500975+hamings@users.noreply.github.com>
2024-07-30 17:48:45 +0900

    KAN-244 test: sms controller

--- ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/customer/dto/request/SmsRequest.java
+++ ubun2_be/src/main/java/kr/or/kosa/ubun2_be/domain/customer/dto/request/SmsRequest.java
@@ -2,13 +2,17 @@ package kr.or.kosa.ubun2_be.domain.customer.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SmsRequest {
 
     @NotBlank(message = "전화번호를 입력해주세요")
-    @Pattern(regexp = "^(010|011|016|017|018|019)\\d{3,4}\\d{4}$", message = "ex) 01012345678")
+    @Pattern(regexp = "^(010|011|016|017|018|019)-\\d{3,4}-\\d{4}$", message = "ex) 010-1234-5678")
     private String phoneNumber;
 
     @NotBlank(message = "이름을 입력해주세요")
